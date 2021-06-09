e7c016fde20d9a2ec3ca14db0a105ba417c630c4
SGeriDEV <sarffygeri@gmail.com>
2021-06-09 16:55:22 +0200

    auto-react & reg handling

--- src/index.js
+++ src/index.js
@@ -12,4 +12,42 @@ client.on("ready", async () => {
   });
 });
 
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
+
+  /* Auto-react for posts in news channel */
+  const newsChannel = client.channels.cache.get("472145151867879444");
+  if (message.channel == newsChannel) {
+    const rockstarEmoji = message.guild.emojis.find(
+      (emoji) => emoji.name === "hurocRockstarGames"
+    );
+    const hurocEmoji = message.guild.emojis.find(
+      (emoji) => emoji.name === "hurocBlack"
+    );
+    await message.react(rockstarEmoji);
+    await message.react(hurocEmoji);
+  }
+
+  /* Registration handling for events */
+  const registrationChannel = client.channels.cache.get("688738820816240692");
+  const eventLogChannel = client.channels.cache.get("594148165402492928");
+
+  if (message.channel == registrationChannel) {
+    if (message.content.startsWith("!jelentkezek")) {
+      const name = message.content.split(" ")[1];
+      if (!name) {
+        message.reply("adj meg egy érvényes játékosnevet!");
+      } else {
+        message.reply("jelentkezésedet fogadtuk, nemsokára meghívunk!");
+        eventLogChannel.send(
+          "<@" + message.author.id + "> felhasználó neve: " + name
+        );
+      }
+    } else if (!message.member.hasPermission("ADMINISTRATOR")) {
+      message.delete(1000);
+    }
+  }
+});
+
 client.login(process.env.TOKEN);
